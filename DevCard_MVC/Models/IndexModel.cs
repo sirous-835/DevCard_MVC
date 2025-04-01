03484632b87c64798ace6ebbadd8cfb50fae3389
Masoud <Masoud@DESKTOP-GIND6SO>
2025-04-01 13:43:49 +0330

    projects section from index page moved to partialview projects

---
+++ DevCard_MVC/Models/IndexModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DevCard_MVC.Models
+{
+	public class IndexModel
+	{
+        public List<project> Projects { get; set; }
+        public List<Article> Articles  { get; set; }
+    }
+}
